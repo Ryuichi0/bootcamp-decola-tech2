236a4e5c3e176dd258de43f90d04c84c0c07005c
ThiagoTNK <thiagotnk11@gmail.com>
2021-12-06 01:53:42 -0300

    aula manipulando array concluída

--- Fundamentos de coleções e LINQ com .NET/Colecoes/Colecoes/Program.cs
+++ Fundamentos de coleções e LINQ com .NET/Colecoes/Colecoes/Program.cs
@@ -1,31 +1,123 @@
-﻿namespace Colecoes
+﻿using Colecoes.Helper;
+
+namespace Colecoes
 {
     class Program
     {
         static void Main(string[] args)
         {
-            int[,] matriz = new int[4, 2]
-            {
-                { 10, 11 },
-                { 20, 21 },
-                { 30, 31 },
-                { 40, 41 }
-            };
+            OperacoesArray op = new OperacoesArray();
 
-            Console.WriteLine("Percorrendo o array pelo for");
-            for (int i = 0; i < matriz.GetLength(0); i++)
-            {
-                for (int j = 0; j < matriz.GetLength(1); j++)
-                {
-                    Console.WriteLine(matriz[i, j]);
-                }
-            }
+            int[] array = new int[5] { 6, 3, 8, 1, 9 };
+            int[] arrayCopia = new int[10];
+            string[] arrayString = op.ConverterParaArrayString(array);
 
-            Console.WriteLine("Percorrendo o array pelo foreach");
-            foreach (int item in matriz)
-            {
-                Console.WriteLine(item);
-            }
+            //int valorProcurado = 9;
+
+            Console.WriteLine($"Capacidade atual do array: {array.Length}");
+            op.RedimensionarArray(ref array, array.Length * 2);
+            Console.WriteLine($"Capacidade atual do array após redimensionar: {array.Length}");
+
+
+
+            //int indice = op.ObterIndice(array, valorProcurado);
+            //if (indice > -1)
+            //{
+            //    Console.WriteLine($"O índice do elemento {valorProcurado} é {indice}");
+            //}
+            //else
+            //{
+            //    Console.WriteLine("Valor não existente no array");
+            //}
+
+
+
+
+            //int valorAchado = op.ObterValor(array, valorProcurado);
+            //if (valorAchado > 0)
+            //{
+            //    Console.WriteLine("Encontrei o valor");
+            //}
+            //else
+            //{
+            //    Console.WriteLine("Não encontrei o valor");
+            //}
+
+
+
+            //bool existe = op.TodosMaiorQue(array, valorProcurado);
+            //if (existe)
+            //{
+            //    Console.WriteLine($"Todos os valores são maiores que {valorProcurado}");
+            //}
+            //else
+            //{
+            //    Console.WriteLine($"Existe valores que não são maiores do que {valorProcurado}");
+            //}
+
+
+
+            //bool existe = op.Existe(array, valorProcurado);
+            //if (existe)
+            //{
+            //    Console.WriteLine($"Encontrei o valor {valorProcurado}");
+            //}
+            //else
+            //{
+            //    Console.WriteLine($"Não encontrei o valor {valorProcurado}");
+            //}
+
+
+
+            //Console.WriteLine("Array antes da cópia:");
+            //op.ImprimirArray(arrayCopia);
+
+            //op.Copiar(ref array, ref arrayCopia);
+            //Console.WriteLine("Array após a cópia");
+            //op.ImprimirArray(arrayCopia);
+
+
+
+            //Console.WriteLine("Array original:");
+            //op.ImprimirArray(array);
+
+            //op.Ordenar(ref array);
+            //Console.WriteLine("Array ordenado");
+            //op.ImprimirArray(array);
+
+
+
+            //Console.WriteLine("Array original:");
+            //op.ImprimirArray(array);
+
+            //op.OrdenarBubbleSort(ref array);
+            //Console.WriteLine("Array ordenado");
+            //op.ImprimirArray(array);
+
+
+
+            //int[,] matriz = new int[4, 2]
+            //{
+            //    { 10, 11 },
+            //    { 20, 21 },
+            //    { 30, 31 },
+            //    { 40, 41 }
+            //};
+
+            //Console.WriteLine("Percorrendo o array pelo for");
+            //for (int i = 0; i < matriz.GetLength(0); i++)
+            //{
+            //    for (int j = 0; j < matriz.GetLength(1); j++)
+            //    {
+            //        Console.WriteLine(matriz[i, j]);
+            //    }
+            //}
+
+            //Console.WriteLine("Percorrendo o array pelo foreach");
+            //foreach (int item in matriz)
+            //{
+            //    Console.WriteLine(item);
+            //}
 
 
 
